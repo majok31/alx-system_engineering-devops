2875dac86a09d1b485816c45186cb10278e89f51
majok31 <majoktot70@gmail.com>
2024-07-01 10:59:35 -0700

    updates

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-#install a specific version of flask
+# Using Puppet, install flask from pip3
 package { 'flask':
-	ensure	=> '2.1.0',
-	provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
